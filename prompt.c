7f2f4c1720c86e49a4b4abd05bcf10e826935c45
Cezary Sałbut <csalbut@gmail.com>
2016-12-10 15:24:11 +0100

    support also windows platform

--- prompt.c
+++ prompt.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#include <string.h>
+
+#define INPUT_BUF_SIZE 2048
+static char buffer[INPUT_BUF_SIZE];
+
+char *readline (char *prompt)
+{
+        fputs(prompt, stdout);
+        fgets(buffer, INPUT_BUF_SIZE, stdin);
+
+        char *usr_input_s = malloc(strlen(buffer) + 1);
+        strcpy(usr_input_s, buffer);
+        usr_input_s[strlen(cpy - 1)] = '\0';
+
+        return usr_input_s;
+}
+
+// stub for WIN32
+void add_history(char* unused) {}
+
+#else // _WIN32
 #include <editline/readline.h>
+#endif // _WIN32
 
 int main(int argc, char** argv) {
 
